c82ae8d1bfaab7b3fa4074a5fcb42fe317250db0
teady.kang <teady.kang@kakaobank.com>
2021-03-09 15:51:43 +0900

    lombok "@Data" apply

--- src/main/java/com/example/javademo/entity/FwkTransactionHstId.java
+++ src/main/java/com/example/javademo/entity/FwkTransactionHstId.java
@@ -1,27 +1,16 @@
 package com.example.javademo.entity;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 // FwkTransactionHst의 Primary Key 객체
+@Data
 public class FwkTransactionHstId implements Serializable {
     @Column LocalDate transactionDate;
     @Column(length = 50) String appName;
     @Column(length = 20) String appVersion;
     @Column(length = 36) String gid;
-
-    public void setTransactionDate(LocalDate transactionDate) { this.transactionDate = transactionDate; }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public void setAppVersion(String appVersion) {
-        this.appVersion = appVersion;
-    }
-
-    public void setGid(String gid) {
-        this.gid = gid;
-    }
 }
